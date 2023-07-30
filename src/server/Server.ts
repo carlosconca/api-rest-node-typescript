de9a5f926924d2570fbc6d15a5f855497639f9b4
Zé Carlos <carlosconca7@gmail.com>
2023-07-30 16:59:56 -0300

    feat: settings cod style

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express from 'express';
 
 const server = express();
 
+interface Teste {
+
+}
+
 server.get('/', (_, res) => {
-    return res.send('olá, DEV!');
+  return res.send('olá, DEV!');
 });
 
-export {server};
+export { server };
